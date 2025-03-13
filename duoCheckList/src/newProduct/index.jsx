4eaac263ca882af788092b2ec929014785afee52
JasonAraujo1 <jasonaraujo123@hotmail.com>
2025-03-13 16:11:21 -0300

    onchange event add

--- duoCheckList/src/newProduct/index.jsx
+++ duoCheckList/src/newProduct/index.jsx
@@ -1,40 +1,54 @@
-import React from 'react'
+import { useState } from "react"
 
 export default function NewProduct() {
+  const [nome, setNome] = useState("")
+  const [status, setStatus] = useState("")
+  const [categoria, setCategoria] = useState("")
+  const [descricao, setDescricao] = useState("")
+
+  function handleClick() {
+    console.log(nome, status, categoria, descricao)
+  }
+
   return (
+
+
+
     <div>
       <button className='text-red-600'>←</button>
       <p className='font-bold'>Novo produto</p>
 
       <div>
         Nome
-        <input className='border' type="text" />
+        <input onChange={(event) => setNome(event.target.value)} className='border' type="text" />
       </div>
 
       <div>
         Status
-        <select name="" id="" className='border'>
+        <select onChange={(event) => setStatus(event.target.value)} className='border'>
           <option selected disabled value="">Categorias</option>
-          <option value="">Adquirido</option>
-          <option value="">Não Adquirido</option>
-          <option value="">Pendente</option>
+          <option value="Adquirido">Adquirido</option>
+          <option value="Não Adquirido">Não Adquirido</option>
         </select>
       </div>
 
       <div>
         Categoria
-        <select name="" id="" className='border'>
+        <select onChange={(event) => setCategoria(event.target.value)} name="" id="" className='border'>
           <option selected disabled value="">Categorias</option>
-          <option value="">Adquirido</option>
-          <option value="">Não Adquirido</option>
-          <option value="">Pendente</option>
+          <option value="Sala">Sala</option>
+          <option value="Quarto">Quarto</option>
+          <option value="Banheiro">Banheiro</option>
+          <option value="Escritório">Escritório</option>
         </select>
       </div>
 
       <div>
         Descrição
-        <input className='border' type="text" />
+        <input onChange={(event) => setDescricao(event.target.value)} className='border' type="text" />
       </div>
+
+      <button onClick={handleClick}>Enviar</button>
     </div>
   )
 }
