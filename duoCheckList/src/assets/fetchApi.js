fc945b51ba6f07fca96334eedc6e00515492b7a7
JasonAraujo1 <jasonaraujo123@hotmail.com>
2025-03-13 19:35:58 -0300

    end point register  check

--- duoCheckList/src/assets/fetchApi.js
+++ duoCheckList/src/assets/fetchApi.js
@@ -1,14 +1,11 @@
-import React from 'react'
 
 export async function fetchApiData() {
- try{
-    const response = await fetch("https://67d2321e90e0670699bca29e.mockapi.io/duoCheckList/data")
-    const data = await response.json()
-    
-    console.log("dados do fetchapidata:", data)
-
-    return data
- }catch(error){
-    console.log("erro no fetch", error)
- }
+   try {
+      const response = await fetch("https://67d2321e90e0670699bca29e.mockapi.io/duoCheckList/data")
+      const data = await response.json()
+      return data
+   } catch (error) {
+      console.log("erro no fetch", error)
+   }
 }
+
--- duoCheckList/src/register/index.jsx
+++ duoCheckList/src/register/index.jsx
@@ -1,6 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function Register() {
+  const [name, setName] = useState("")
+  const [password, setPassword] = useState("")
+  const [repeatPassword, setRepeatPassword] = useState("")
+
+
+  async function handleClick() {
+    const data = {
+      name: name,
+      password: password
+    }
+    const url = "https://67d355c78bca322cc269d90d.mockapi.io/register/register"
+
+    const req = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    })
+    const res = await req.json()
+    // console.log(res)
+
+    if (password !== repeatPassword) {
+      alert("As senhas não coincidem!");
+     
+    }else{
+      alert("Cadastro efetuado!")
+    }
+  }
 
   return (
     <div>
@@ -10,25 +39,26 @@ export default function Register() {
       </div>
 
       <div>
-      <p className='font-bold text-xl'>Criar conta</p>
+        <p className='font-bold text-xl'>Criar conta</p>
         <span>preencha os campos para criar</span>
       </div>
 
       <div>
         <span>nome de usuário: </span>
-        <input className='border' type="text" />
+        <input onChange={(event) => setName(event.target.value)} className='border' type="text" />
       </div>
 
       <div>
         <span>Senha: </span>
-        <input className='border' type="password" />
+        <input onChange={(event) => setPassword(event.target.value)} className='border' type="password" />
       </div>
 
       <div>
         <span>Repetir senha: </span>
-        <input className='border' type="password" />
+        <input onChange={(event) => setRepeatPassword(event.target.value)} className='border' type="password" />
       </div>
 
+      <button onClick={handleClick}>Enviar</button>
     </div>
   )
 }
