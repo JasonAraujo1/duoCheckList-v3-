928603fac3589d052ca487a2e4077775badab554
JasonAraujo1 <jasonaraujo123@hotmail.com>
2025-03-24 20:06:18 -0300

    home style tailwind adjust

--- duoCheckList/src/home/index.jsx
+++ duoCheckList/src/home/index.jsx
@@ -57,73 +57,77 @@ export default function Home() {
   return (
     <div>
 
-      <div className='flex'>
+
+      <div className='flex mb-8'>
         <NavLink to="/new">
-          <img src={plus} alt="" className='' />
+          <img src={plus} alt="" className='size-15' />
           <span className='text-gray-400 text-sm'>Novo</span>
         </NavLink>
 
       </div>
+      <div className='flex flex-col items-center'>
 
-      <div>
-        <img src={search} alt="" className='' />
-        <input value={searchProduct} onChange={handleChange} placeholder='Buscar produto'
-          className='bg-neutral-100 text-center' type="text" />
-      </div>
 
-      <div className="overflow-x-auto max-w-full">
-        <table className="min-w-full border text-sm md:text-base">
-          <thead className='border'>
-            <tr>
-              <th className="px-2 py-2">Todos</th>
-              <th className="px-2 py-2">
-                <select onChange={handleSelect} className="border rounded px-1 py-1 text-xs md:text-sm">
-                  <option selected disabled value="categorias">Categorias</option>
-                  <option value="Sala">Sala</option>
-                  <option value="Quarto">Quarto</option>
-                  <option value="Banheiro">Banheiro</option>
-                  <option value="Lavanderia">Lavanderia</option>
-                  <option value="Escritório">Escritório</option>
-                  <option value="Quintal/Jardim">Quintal/Jardim</option>
-                  <option value="Varanda/Sacada">Varanda/Sacada</option>
-                  <option value="Cozinha">Cozinha</option>
-                  <option value="Todos">Todos</option>
-                </select>
-              </th>
-              <th className="px-2 py-2">
-                <select onChange={handleSelectStatus} className="border rounded px-1 py-1 text-xs md:text-sm">
-                  <option selected disabled value="categorias">Status</option>
-                  <option value="Adquirido">Adquirido</option>
-                  <option value="Não Adquirido">Não Adquirido</option>
-                  <option value="Todos">Todos</option>
-                </select>
-              </th>
-            </tr>
-          </thead>
+        <div className='bg-neutral-100 flex p-2 rounded-xl justify-center w-full max-w-80 mb-5'>
+          <img src={search} alt="" className='size-7 ' />
+          <input value={searchProduct} onChange={handleChange} placeholder='Buscar Produto...'
+            className=' text-center outline-0' type="text" />
+        </div>
 
-          <tbody className='border'>
-            {display.length === 0 ? (
+        <div className="overflow-x-auto max-w-full">
+          <table className="min-w-full text-sm md:text-base">
+            <thead className='border-b-2 border-b-red-400 '>
               <tr>
-                <td colSpan="3" className="text-center py-2">Nenhum produto encontrado.</td>
+                <th className="px-2 py-2 text-red-400 text-base">Todos</th>
+                <th className="px-2 py-2">
+                  <select onChange={handleSelect} className="text-red-400  rounded px-1 py-1 text-xs md:text-sm">
+                    <option selected disabled value="categorias">Categorias</option>
+                    <option value="Sala">Sala</option>
+                    <option value="Quarto">Quarto</option>
+                    <option value="Banheiro">Banheiro</option>
+                    <option value="Lavanderia">Lavanderia</option>
+                    <option value="Escritório">Escritório</option>
+                    <option value="Quintal/Jardim">Quintal/Jardim</option>
+                    <option value="Varanda/Sacada">Varanda/Sacada</option>
+                    <option value="Cozinha">Cozinha</option>
+                    <option value="Todos">Todos</option>
+                  </select>
+                </th>
+                <th className="px-2 py-2">
+                  <select onChange={handleSelectStatus} className="text-red-400 rounded px-1 py-1 text-xs md:text-sm">
+                    <option selected disabled value="categorias">Status</option>
+                    <option value="Adquirido">Adquirido</option>
+                    <option value="Não Adquirido">Não Adquirido</option>
+                    <option value="Todos">Todos</option>
+                  </select>
+                </th>
               </tr>
-            ) : (
-              display.map((item) => (
-                <tr key={item.id} className="hover:bg-gray-100">
-                  <td className='cursor-pointer px-2 py-1'>
-                    <NavLink to="/product" onClick={() => handleClickProduct(item.id)} className="text-blue-600 hover:underline">
-                      {item.product}
-                    </NavLink>
-                  </td>
-                  <td className='px-2 py-1'>{item.category}</td>
-                  <td className='px-2 py-1'>{item.status}</td>
+            </thead>
+
+            <tbody className=''>
+              {display.length === 0 ? (
+                <tr>
+                  <td colSpan="3" className="text-center py-2">Nenhum produto encontrado.</td>
                 </tr>
-              ))
-            )}
-          </tbody>
-        </table>
+              ) : (
+                display.map((item) => (
+                  <tr key={item.id} className="hover:bg-gray-100">
+                    <td className='cursor-pointer px-2 py-1'>
+                      <NavLink to="/product" onClick={() => handleClickProduct(item.id)} className="text-blue-600 hover:underline">
+                        {item.product}
+                      </NavLink>
+                    </td>
+                    <td className='px-2 py-1'>{item.category}</td>
+                    <td className='px-2 py-1'>{item.status}</td>
+                  </tr>
+                ))
+              )}
+            </tbody>
+          </table>
+        </div>
+
+
       </div>
-
-
     </div>
   )
 }
