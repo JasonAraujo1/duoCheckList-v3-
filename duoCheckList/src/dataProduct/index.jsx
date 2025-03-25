803566987bb8f7f41318da772299cec7b572c0a3
JasonAraujo1 <jasonaraujo123@hotmail.com>
2025-03-25 17:35:56 -0300

    product page stylized

--- duoCheckList/src/dataProduct/index.jsx
+++ duoCheckList/src/dataProduct/index.jsx
@@ -1,6 +1,10 @@
 import { useEffect, useState } from 'react';
 import { NavLink } from 'react-router';
-import { fetchApiIdUser, fetchApiProducts } from '../services/fetchApi'; // certifique-se que está importado
+import { fetchApiIdUser } from '../services/fetchApi';
+import arrow from '../assets/arrow.svg'
+import trash from '../assets/trash.svg'
+import edit from '../assets/edit.svg'
+
 
 export default function DataProduct() {
   const [dataUser, setDataUser] = useState([]);
@@ -15,51 +19,56 @@ export default function DataProduct() {
       const idProduct = localStorage.getItem("idProduct")
       // console.log("idProduct", idProduct)
       setDataProduct(idProduct)
-      
+
     }
     onLoad()
   }, []);
   // console.log("dataUser",dataUser)
-  
+
   const filteredProduct = dataUser.filter((item) => item.id === dataProduct);
-  console.log("produto filtrado",filteredProduct);
+  console.log("produto filtrado", filteredProduct);
 
   return (
     <div>
       <NavLink to={"/home"}>
-        <button className='text-red-600'>←</button>
+        <img src={arrow} alt="" className='size-9' />
       </NavLink>
-      <p className='font-bold'>Dados do produto</p>
+      <p className='font-bold py-16 text-base'>Dados do produto</p>
 
       <div>
         {dataUser.length === 0 ? (
           <p>Nenhum produto encontrado.</p>
         ) : (
           filteredProduct.map((item) => (
-            <table key={item.id} className='w-sm mb-4 border'>
-              <tbody>
-                <tr className='border'>
-                  <td>Nome</td>
-                  <td>{item.product}</td>
+            <table key={item.id} className="w-full sm:w-auto mb-8  table-auto ">
+              <tbody >
+                <tr className="border  border-gray-200">
+                  <td className="px-4 py-2 bg-gray-100">Nome</td>
+                  <td className="px-4 py-2">{item.product}</td>
                 </tr>
-                <tr className='border'>
-                  <td>Status</td>
-                  <td>{item.status}</td>
+                <tr className="border border-gray-200">
+                  <td className="px-4 py-2 bg-gray-100">Status</td>
+                  <td className={`px-4 py-2 ${item.status === 'Não Adquirido' ? 'text-amber-500' : 'text-lime-500'
+                        }`}>{item.status}</td>
                 </tr>
-                <tr className='border'>
-                  <td>Categoria</td>
-                  <td>{item.category}</td>
+                <tr className="border border-gray-200">
+                  <td className="px-4 py-2 bg-gray-100">Categoria</td>
+                  <td className="px-4 py-2">{item.category}</td>
                 </tr>
-                <tr className='border'>
-                  <td>Descrição</td>
-                  <td>{item.description}</td>
+                <tr className="border border-gray-200">
+                  <td className="px-4 py-2 bg-gray-100">Descrição</td>
+                  <td className="px-4 py-2">{item.description}</td>
                 </tr>
               </tbody>
             </table>
+
+
           ))
         )}
-        <button>Apagar</button>
-        <button>Editar</button>
+        <div className='flex justify-between'>
+          <img src={trash} alt="" className='bg-red-400 rounded-lg p-1 size-8 ' />
+          <img src={edit} alt="" className='bg-red-400 rounded-lg p-1 size-8 ' />
+        </div>
       </div>
     </div>
   );
