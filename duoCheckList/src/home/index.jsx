2d50803f6007dea77a68965323e5a54363dc77b1
JasonAraujo1 <jasonaraujo123@hotmail.com>
2025-03-26 16:38:11 -0300

    input search adjust width

--- duoCheckList/src/home/index.jsx
+++ duoCheckList/src/home/index.jsx
@@ -72,7 +72,7 @@ export default function Home() {
       <div className='flex flex-col items-center'>
 
 
-        <div className='bg-neutral-100 flex p-2 rounded-lg justify-center w-full max-w-80 mb-8 gap-4'>
+        <div className='bg-neutral-100 flex p-2 rounded-lg justify-center w-full max-w-80 mt-5 mb-8 gap-4'>
           <img src={search} alt="" className='size-7 ' />
           <input value={searchProduct} onChange={handleChange} placeholder='Buscar Produto...'
             className='  outline-0' type="text" />
