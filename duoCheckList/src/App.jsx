c5479e11ac90c6db9381909a050a3765a6abd3ca
JasonAraujo1 <jasonaraujo123@hotmail.com>
2025-04-01 17:57:44 -0300

    index home element

--- duoCheckList/src/App.jsx
+++ duoCheckList/src/App.jsx
@@ -12,7 +12,7 @@ export default function App() {
 
   return (
     <Routes>
-      <Route path ="/home" element={<Home/>}/>
+      <Route index element={<Home/>}/>
       <Route path ="/login" element={<Login/>}/>
       <Route path ="/register" element={<Register/>}/>
       <Route path ="/product" element={<DataProduct/>}/>
