63e8bedd4f0fa09f3c8e68885455f5cd8cd0194a
JasonAraujo1 <jasonaraujo123@hotmail.com>
2025-04-01 18:06:46 -0300

    try vercel adjust login home

--- duoCheckList/src/App.jsx
+++ duoCheckList/src/App.jsx
@@ -1,19 +1,19 @@
 import { Route, Routes } from 'react-router'
 import './App.css'
-import Home from './home'
 import Login from './login'
 import Register from './register'
 import DataProduct from './dataProduct'
 import NewProduct from './newProduct'
 import Edit from './editProduct'
+import Home from './home';
 
 
 export default function App() {
 
   return (
     <Routes>
-      <Route index element={<Home/>}/>
-      <Route path ="/login" element={<Login/>}/>
+      <Route path="/home" element={<Home/>}/>
+      <Route index element={<Login/>}/>
       <Route path ="/register" element={<Register/>}/>
       <Route path ="/product" element={<DataProduct/>}/>
       <Route path ="/new" element={<NewProduct/>}/>
