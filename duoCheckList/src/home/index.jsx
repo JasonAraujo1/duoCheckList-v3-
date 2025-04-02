baf201a015113604d8de567244a584ce2fbe1d79
JasonAraujo1 <jasonaraujo123@hotmail.com>
2025-04-02 20:38:00 -0300

    user name dinamic home

--- duoCheckList/src/home/index.jsx
+++ duoCheckList/src/home/index.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { fetchApiIdUser } from '../services/fetchApi'
+import { fetchApiIdUser, fetchApiUsers } from '../services/fetchApi'
 import { NavLink, useNavigate } from 'react-router'
 import plus from '../assets/plus.svg';
 import search from '../assets/search.svg';
@@ -15,10 +15,11 @@ export default function Home() {
   const [cleanSelection, setCleanSelection] = useState(false);
   const [selectedCategory, setSelectedCategory] = useState("");
   const [selectedStatus, setSelectedStatus] = useState("");
+  const [userName, setUserName] = useState([]);
   const navigate = useNavigate();
 
 
-  function handleLogout(){
+  function handleLogout() {
     localStorage.clear()
     navigate("/")
   }
@@ -26,19 +27,23 @@ export default function Home() {
   useEffect(() => {
     async function onLoad() {
       const idUser = localStorage.getItem("idUser")
-      if(idUser){
+      if (idUser) {
         const data = await fetchApiIdUser(idUser)
         setDataProduct(Array.isArray(data) ? data : [])
         setDisplay(Array.isArray(data) ? data : [])
+        
       }
-      else{
+      else {
         setDisplay([])
         navigate("/")
       }
       //  console.log("idUser", idUser)
       //Se o usuário não tem produtos, a API pode retornar algo que não é um array (como undefined, null ou {}). Quando isso acontece, o React tenta fazer dataProduct.map(...) em algo inválido, o que gera erro e quebra a página.
       //então usamos Array.isArray() para checar se data é um array. Se for, usamos ele. Se não for, usamos um array vazio [].
-
+      const user = await fetchApiUsers()
+      const userNamefilter = user.filter((item)=>(item.id).includes(idUser))
+      const userNameChoosed = userNamefilter.map((item)=>(item.name))
+      setUserName(userNameChoosed)
     }
     onLoad()
   }, [])
@@ -48,7 +53,7 @@ export default function Home() {
     setSearchProduct(target.value)
     const filterInput = dataProduct.filter((item) => item.product.includes(searchProduct))
     setDisplay(filterInput)
-    if(target.value === ""){
+    if (target.value === "") {
       setDisplay(dataProduct)
     }
   }
@@ -79,8 +84,8 @@ export default function Home() {
     setCleanSelection(false)
   }
   //toda vez que usa as funcoes de select options, atualiza o status da 'cleanselection' com boolean, fiz o ternario para ativar o 'limpar filtros' em caso de true no html(linha 85), e ao clicar no limpar filtro ele atualiza o staus do setdisplay com data product e o status do cleanselection com false para esconder o 'limpar filtros'
-  
-  
+
+
   return (
     <div className='w-full sm:w-90 md:w-200'>
 
@@ -93,10 +98,13 @@ export default function Home() {
           <img src={logout} alt="" className='size-8 hover:' />
           <span className='text-gray-400 text-sm cursor-pointer'>Sair</span>
         </div>
-
       </div>
-      <div className='flex flex-col items-center'>
 
+      <div className='flex mb-3 mx-4'>
+        <span className='text-gray-400 text-medium font-medium'>Olá, {userName}</span>
+      </div>
+
+      <div className='flex flex-col items-center'>
 
         <div className='bg-neutral-100 flex p-2 rounded-lg justify-center w-full max-w-80 mt-5 mb-6 gap-4'>
           <img src={search} alt="" className='size-7 ' />
@@ -105,7 +113,7 @@ export default function Home() {
         </div>
 
         <div className='w-full text-start cursor-pointer  h-4' onClick={handleClean}>
-          <span className={`underline text-sm flex items-center gap-1 ${cleanSelection === false ? 'hidden disabled ' : 'text-gray-500'}`}>Limpar filtros <span><img src={clean} alt="" className='size-3.5'/></span></span>
+          <span className={`underline text-sm flex items-center gap-1 ${cleanSelection === false ? 'hidden disabled ' : 'text-gray-500'}`}>Limpar filtros <span><img src={clean} alt="" className='size-3.5' /></span></span>
         </div>
 
         <div className=" ">
@@ -114,7 +122,7 @@ export default function Home() {
               <tr className=''>
                 <th className="w-30 md:w-72 text-start px-2 py-2 text-red-400 text-base">Produtos</th>
                 <th className="text-start py-2 pr-3">
-                  <select value={selectedCategory}  onChange={handleSelect} className="text-red-400  rounded  py-1  text-base md:text-sm outline-0 w-full md:w-36" >
+                  <select value={selectedCategory} onChange={handleSelect} className="text-red-400  rounded  py-1  text-base md:text-sm outline-0 w-full md:w-36" >
                     <option disabled value="">Categorias</option>
                     <option value="Sala">Sala</option>
                     <option value="Quarto">Quarto</option>
