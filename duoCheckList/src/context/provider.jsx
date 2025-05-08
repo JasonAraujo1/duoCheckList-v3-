f74530fc4ca2cf533fb7022605679a4b98bcdd91
JasonAraujo1 <jasonaraujo123@hotmail.com>
2025-05-08 11:57:03 -0300

    provider data adjust

--- duoCheckList/src/context/provider.jsx
+++ duoCheckList/src/context/provider.jsx
@@ -1,14 +1,22 @@
 import Context from './context';
 import { useState } from "react";
 
-function Provider({ children }) {
-    const [valorGlobal, setValorGlobal] = useState("tipo de dado que você quiser");
+async function Provider({ children }) {
+    const [name, setName] = useState("")
+    const [password, setPassword] = useState("")
+    const [userId, setUserId] = useState([]);
 
 
+    const data = await fetchApiUsers()
+    const dataName = data.name;
+    const dataPassword = data.password;
+    const dataId = data.id;
+    
 
     const contextValue = {
-      valorGlobal,
-      setValorGlobal
+        name,
+        password,
+        userId
     };
 
 
